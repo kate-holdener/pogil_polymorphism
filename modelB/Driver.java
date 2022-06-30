41ea01ee9ded6e7887ac0ac460e8707e07b9e4f1
Kate Holdener <kate.holdener@slu.edu>
2022-06-30 08:44:53 -0500

    added printDistance method

--- modelB/Driver.java
+++ modelB/Driver.java
@@ -1,5 +1,11 @@
 public class Driver
 {
+   public static void printDistance(Point point)
+   {
+      double distance = point.getDistanceFromZero();
+      System.out.println("Distance from point " + point + " to zero is " + distance);
+   } 
+
    public static void main(String []args)
    {
       Point center = new Point(0,0);
@@ -7,6 +13,6 @@ public class Driver
       System.out.println("Center's Y value is " + center.getY());
 
       Point3D start = new Point3D(10,10,10);
-      // add your code for question 12 below
+      // add code for your experiments below
    }
 }
